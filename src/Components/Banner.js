2e724714499e1c157c3d043b8a69e20eeb55b77a
Nico Ihle <n.ihle2003@gmail.com>
2022-12-04 01:15:49 +0100

    UPDATE: CSS

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -66,13 +66,13 @@ export default function Home() {
                             <div className='welcometext'>
                                 <p className='welcome-inhalt-p'>Hello,</p>
                                 <h1 className='welcome-inhalt-h1 '>I'm Nico Ihle</h1>
-                                <p className='welcome-inhalt-p'>{`I love to construct `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></p>
+                                <p className='welcome-inhalt-p'>{`I love to build `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></p>
                                 {/* <p className='welcome-inhalt-p'>i love to create things...</p> */}
                                 {/* <Typical className="welcome-inhalt-p" steps={['I love to build,', 2000, 'APPS & IOT ', 2000, 'projekts', 3000,]} loop={Infinity} /> */}
                             </div>
 
                         </div>
-                        
+
                         <div className='welcomeblock2'>
                             <div class="iphonex">
                                 <div class="front">
